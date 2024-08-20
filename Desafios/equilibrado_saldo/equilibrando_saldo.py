8a995007400377f61c324d846bd99c19f67523c8
rayLorena <raylamatias123@gmail.com>
2024-08-20 19:29:35 -0300

    first commit

---
+++ Desafios/equilibrado_saldo/equilibrando_saldo.py
@@ -0,0 +1,9 @@
+saldo_atual = float(input())
+valor_deposito = float(input())
+valor_retirada = float(input())
+
+
+resultado = (saldo_atual + valor_deposito) - valor_retirada
+
+resultado = (saldo_atual + valor_deposito) - valor_retirada
+print(f'Saldo atualizado na conta: {resultado:.1f}')
